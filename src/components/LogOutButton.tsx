308849ce1041bbaa25e26d18bc714daacf6e99d5
sheranbuckmangit <sherantuanbuckman@gmail.com>
2025-06-02 00:12:37 +0530

    feat: log in button

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -4,7 +4,8 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+    onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
       Log Out
     </button>
   );
